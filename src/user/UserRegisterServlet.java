c1ee969f7b6ea83fdb2caf5ce2a0811ae4c3fadf
YPG <ypgcute@gmail.com>
2021-04-08 14:55:10 +0900

    editin userservlet

--- src/user/UserRegisterServlet.java
+++ src/user/UserRegisterServlet.java
@@ -1,7 +1,6 @@
 package user;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,34 +8,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/UserSearchServlet") 
-public class UserSearchServlet extends HttpServlet {
+/**
+ * Servlet implementation class UserRegisterServlet
+ */
+@WebServlet("/UserRegisterServlet")
+public class UserRegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
 		String userName = request.getParameter("userName");
-		response.getWriter().write(getJSON(userName));
+		String userAge = request.getParameter("userAge");
+		String userGender = request.getParameter("userGender");
+		String userEmail = request.getParameter("userEmail");
+		response.getWriter().write(register(userName, userAge, userGender, userEmail));
+		
+		
 	}
-	
-	public String getJSON(String userName) {
-		if(userName == null) {
-			userName = "";
+		public int register(String userName, String userAge, String userGEnder, String userEmail) {
+			
 		}
-			StringBuffer result = new StringBuffer("");
-			result.append("{\"result\":[");
-			UserDAO userDAO = new UserDAO();
-			ArrayList<User> userList = userDAO.search(userName);
-			for(int i = 0; i < userList.size(); i++) {
-				result.append("[{\"value\": \"" + userList.get(i).getUserName() + "\"},");
-				result.append("{\"value\": \"" + userList.get(i).getUserAge() + "\"},");
-				result.append("{\"value\": \"" + userList.get(i).getUserGender() + "\"},");
-				result.append("{\"value\": \"" + userList.get(i).getUserEmail() + "\"}],");
-			}
-			result.append("]}");
-			return result.toString();
-		}
-	}
 
 }
