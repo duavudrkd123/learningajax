9a94cf32712a75506e7f63210f72daf90a5fed35
Lee Jun Gyu <leejg@renosoft.kr>
2021-04-08 15:08:06 +0900

    fix search & register servlet.java

--- src/user/UserRegisterServlet.java
+++ src/user/UserRegisterServlet.java
@@ -1,42 +1,42 @@
 package user;
 
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/UserSearchServlet") 
-public class UserSearchServlet extends HttpServlet {
+import jdk.vm.ci.code.Register;
+
+/**
+ * Servlet implementation class UserRegisterServlet
+ */
+@WebServlet("/UserRegisterServlet")
+public class UserRegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
 		String userName = request.getParameter("userName");
-		response.getWriter().write(getJSON(userName));
-	}
-	
-	public String getJSON(String userName) {
-		if(userName == null) {
-			userName = "";
-		}
-			StringBuffer result = new StringBuffer("");
-			result.append("{\"result\":[");
-			UserDAO userDAO = new UserDAO();
-			ArrayList<User> userList = userDAO.search(userName);
-			for(int i = 0; i < userList.size(); i++) {
-				result.append("[{\"value\": \"" + userList.get(i).getUserName() + "\"},");
-				result.append("{\"value\": \"" + userList.get(i).getUserAge() + "\"},");
-				result.append("{\"value\": \"" + userList.get(i).getUserGender() + "\"},");
-				result.append("{\"value\": \"" + userList.get(i).getUserEmail() + "\"}],");
-			}
-			result.append("]}");
-			return result.toString();
-		}
+		String userAge = request.getParameter("userAge");
+		String userGender = request.getParameter("userGender");
+		String userEmail = request.getParameter("userEmail");
+		response.getWriter().write(register(userName, userAge, userGender, userEmail));
 	}
 
+	
+	public int register(String userName, String userAge, String userGender, String userEmail) {
+		User user = new User();
+		try {
+			user.setUserName(userName);
+			user.setUserAge(Integer.parseInt(userAge));
+			user.setUserGender(userGender);
+			user.setUserEmail(userEmail);
+		} catch (Exception e) {
+			return 0;
+		}
+		return new UserDAO().register(user);
+	}
 }
